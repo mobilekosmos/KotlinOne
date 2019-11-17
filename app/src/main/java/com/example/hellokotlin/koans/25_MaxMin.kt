0f18edd5e3ff69b24f72e6d7ed478c4ec8293c93
mobilekosmos <mr.davidbauer@gmail.com>
2019-11-17 01:24:37 +0100

    + koan 25

---
+++ app/src/main/java/com/example/hellokotlin/koans/25_MaxMin.kt
@@ -0,0 +1,14 @@
+package com.example.hellokotlin.koans
+
+/*
+Max; min
+Implement Shop.getCustomerWithMaximumNumberOfOrders() and Customer.getMostExpensiveOrderedProduct() using max, min, maxBy, or minBy.
+
+listOf(1, 42, 4).max() == 42
+listOf("a", "ab").minBy { it.length } == "a"
+ */
+// Return a customer whose order count is the highest among all customers
+fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? = customers.maxBy {it.orders.size}
+
+// Return the most expensive product which has been ordered
+fun Customer.getMostExpensiveOrderedProduct(): Product? = orders.flatMap {it.products}.maxBy{it.price}
